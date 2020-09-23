6bd617287ff1f0c66c71657dbe8a5db987071d5c
melinda.magyar@stud.uni-corvinus.hu <melinda.magyar@stud.uni-corvinus.hu>
2020-09-23 19:32:56 +0200

    fajlba

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -33,5 +33,10 @@ namespace UserMaintenance
             };
             users.Add(u);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Fajl");
+        }
     }
 }
